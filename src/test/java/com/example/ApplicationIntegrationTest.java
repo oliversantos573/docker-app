4f52705f3101e7b9c7906c8656d0fd167948ca06
Oliver Santos <zabsoluto234@gmail.com>
2026-06-26 18:20:16 -0300

    Ajusta UserControllerTest para usar construtor com phoneNumber

--- src/test/java/com/example/ApplicationIntegrationTest.java
+++ src/test/java/com/example/ApplicationIntegrationTest.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+
 @Testcontainers
+@SpringBootTest(properties = "spring.profiles.active=test")
+
 public class ApplicationIntegrationTest {
 
     @Container
